a0f05764361516e8bc88740c24f65eac0058c739
Jewel <jewelranar420@gmail.com>
2024-06-03 08:03:58 +0600

    remove key from unsorted array

--- src/com/jewel/patterns/twopointers/easy/RemoveDuplicate.java
+++ src/com/jewel/patterns/twopointers/easy/RemoveDuplicate.java
@@ -14,10 +14,26 @@ public class RemoveDuplicate {
         return start+1;
     }
 
+    private static int removeDuplicateFromUnsortedArray(int arr[], int target){
+        int start = 0;
+
+        for (int end = 0; end < arr.length; end++){
+            if (arr[end] != target){
+                arr[start] = arr[end];
+                start++;
+            }
+        }
+        return start;
+    }
+
 
     public static void main(String[] args) {
         int[] arr = new int[]{2,3,3,3,9,9};
         int[] arr2 = new int[]{2,2,2,11};
+
+        int[] arr3 = new int[]{3,2,3,7,8,1,3,3};
+
+        System.out.println(removeDuplicateFromUnsortedArray(arr3, 3));
 
         System.out.println(removeDuplicate(arr));
 
